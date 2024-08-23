f42abedcc4562ab2a1f62244017718c221fe1edc
levisantn <43606830+levisantn@users.noreply.github.com>
2024-08-23 23:52:31 +0200

    modify dependencies, app scripts, add prettier config files, add eslint config files, add vite config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { ChakraProvider, theme } from "@chakra-ui/react";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import App from './App.jsx';
+import './index.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
--- src/theme.js
+++ src/theme.js
@@ -1,31 +1,31 @@
 // theme.js
 
 export const flatDarkTheme = {
-  body: "#17202a",
-  text: "#abb2b9",
-  expTxtColor: "#000a12",
-  highlight: "#273746",
-  dark: "#000000",
-  secondaryText: "#808b96",
-  imageHighlight: "#7b241c",
-  compImgHighlight: "#808b96",
-  jacketColor: "#566573",
-  headerColor: "#273746",
-  splashBg: "#abb2b9",
+  body: '#17202a',
+  text: '#abb2b9',
+  expTxtColor: '#000a12',
+  highlight: '#273746',
+  dark: '#000000',
+  secondaryText: '#808b96',
+  imageHighlight: '#7b241c',
+  compImgHighlight: '#808b96',
+  jacketColor: '#566573',
+  headerColor: '#273746',
+  splashBg: '#abb2b9',
 };
 
 export const flatLightTheme = {
-  body: "#EAECEE",
-  text: "#212f3d",
-  expTxtColor: "#000a12",
-  highlight: "#d5d8dc",
-  dark: "#000000",
-  secondaryText: "#566573",
-  imageHighlight: "#922b21",
-  compImgHighlight: "#808b96",
-  jacketColor: "#566573",
-  headerColor: "#d5d8dc",
-  splashBg: "#212f3d",
+  body: '#EAECEE',
+  text: '#212f3d',
+  expTxtColor: '#000a12',
+  highlight: '#d5d8dc',
+  dark: '#000000',
+  secondaryText: '#566573',
+  imageHighlight: '#922b21',
+  compImgHighlight: '#808b96',
+  jacketColor: '#566573',
+  headerColor: '#d5d8dc',
+  splashBg: '#212f3d',
 };
 
 export const chosenTheme = flatDarkTheme;
