c14d3f4ab891f55041c0d2e333eed35ec9a345d9
levisantn <43606830+levisantn@users.noreply.github.com>
2024-08-23 19:53:26 +0200

    setup react website init files, add project dependencies, dev dependencies, scripts, add themes

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import App from "./App.jsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+);
