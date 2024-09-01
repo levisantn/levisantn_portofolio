87d0dbb50220030b2e2aea01419842b92c405f87
levisantn <43606830+levisantn@users.noreply.github.com>
2024-09-01 23:04:13 +0200

    add jetbrains mono font, styles,css: font, normalize, theme, set default styles,

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, theme } from '@chakra-ui/react';
 import App from './App.jsx';
 import './main.css';
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </StrictMode>,
 );
