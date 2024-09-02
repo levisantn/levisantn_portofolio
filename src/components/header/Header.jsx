3a6a399698894f21c1fc8d6c6b6554f6f3fd28a9
levisantn <43606830+levisantn@users.noreply.github.com>
2024-09-03 00:21:28 +0200

    add header/appBar template

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Header = () => {
+  return (
+    <AppBar position='static' color='primary'>
+      <Toolbar>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          LOGO
+        </Typography>
+        <Button color='inherit'>Home</Button>
+        <Button color='inherit'>About me</Button>
+        <Button color='inherit'>Playground</Button>
+        <Button color='inherit'>Projects</Button>
+        <Button color='inherit'>Cheatsheets</Button>
+        <Button color='inherit'>References</Button>
+        <Button color='inherit'>Contact me</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 // import * as React from 'react';
 import { Grid2 } from '@mui/material';
+import Header from '../components/header/Header';
 
 function MainLayout() {
   return (
     <Grid2 container spacing={0}>
-      <Grid2 size={12} sx={{ backgroundColor: 'blue' }}>
-        <h2>Navigation Bar</h2>
+      <Grid2 size={12}>
+        <Header />
       </Grid2>
       <Grid2 size={12} sx={{ backgroundColor: 'yellow' }}>
         <h1>Main content</h1>
